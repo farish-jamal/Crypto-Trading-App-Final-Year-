12a61f19f2579dd857a77c8d7d10d3f30d3309b1
Farish <farishjamal98@gmail.com>
2022-03-02 23:38:29 +0530

    Header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import './App.css';
+import { makeStyles } from '@material-ui/core';
 import Header from "./Component/Header";
 import CoinPage from "./Pages/CoinPage";
 import HomePage from "./Pages/HomePage";
 
 function App() {
+  const useStyles = makeStyles(() =>({
+    App:{
+      backgroundColor : "#000",
+      color: "#fff",
+      minHeight: "100vh"
+  }
+  }))
+
+  const classes = useStyles();
   return (
     // <BrowserRouter>
-       <div>
+       <div className={classes.App}>
          <Header />
          <Routes >
            <Route path="/" element={<HomePage />}  exact />
--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { Container, makeStyles, MenuItem, Select } from '@material-ui/core';
 
-function Header() {
+const useStyles = makeStyles(() =>({
+    Title:{
+      color: "gold",
+      flex: 1,
+      fontWeight: "bold",
+      cursor: "pointer"
+  }
+  }))
+  
+  function Header() {
+  const classes = useStyles();
   return (
-    <div>Header</div>
+    <AppBar position="static" color='transparent'>
+      <Container >
+        <Toolbar>
+          <Typography variant='h6' className={classes.Title}>
+            Crypto Hunter
+          </Typography>
+          <Select variant='outlined' style={{
+            height: 40,
+            width: 100,
+            marginRight: 15
+          }}>
+            <MenuItem value="USD">USD</MenuItem>
+            <MenuItem value="INR">INR</MenuItem>
+          </Select>
+        </Toolbar>
+      </Container>
+    </AppBar>
   )
 }
 
