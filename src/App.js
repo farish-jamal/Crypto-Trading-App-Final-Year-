66717b6f92b5fe7eb6398d0ab5df26c6ec5c5981
Farish <farishjamal98@gmail.com>
2022-03-02 23:01:50 +0530

    Routing in components setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom"
 import './App.css';
+import Header from "./Component/Header";
+import CoinPage from "./Pages/CoinPage";
+import HomePage from "./Pages/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    // <BrowserRouter>
+       <div>
+         <Header />
+         <Routes >
+           <Route path="/" element={<HomePage />}  exact />
+           <Route path="/coin/:id" element={<CoinPage />} />
+         </Routes>
+       </div>
+    // </BrowserRouter>
   );
 }
 
